c10a3ca6dfb249e97e7bf55dc0c6f1779de6b6fe
ivancap7777 <ivancap7777@gmail.com>
2026-02-10 16:28:52 +0000

    root

--- remotion/Root.jsx
+++ remotion/Root.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Composition } from 'remotion';
-import { MyComposition } from './Composition.jsx';
+import React from "react";
+import { Composition } from "remotion";
+import { MyComposition } from "./Composition.jsx";
 
-export const RemotionRoot = () => {
-    return (
-        <>
-            <Composition
-                id="Empty"
-                component={MyComposition}
-                durationInFrames={3300}
-                fps={30}
-                width={360}
-                height={640}
-            />
-        </>
-    );
+const RemotionRoot = () => {
+  return (
+    <>
+      <Composition
+        id="Empty"
+        component={MyComposition}
+        durationInFrames={3300}
+        fps={30}
+        width={360}
+        height={640}
+      />
+    </>
+  );
 };
+
+export default RemotionRoot;
