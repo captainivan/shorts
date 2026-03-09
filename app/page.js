0e4a41072d5191f48e9e82b3c0e5bb52a99aa122
ivancap7777 <ivancap7777@gmail.com>
2026-03-09 08:18:02 +0000

    the yml update

--- app/page.js
+++ app/page.js
@@ -1,31 +1,9 @@
-"use client"
-import { Player } from '@remotion/player';
-import { MyComposition } from '@/remotion/Composition.jsx';
-import subtitles from '@/subtitle/subtitle.json';
-import { useState } from 'react';
-
 export default function Home() {
-  const [number, setNumber] = useState()
-  const handleCheck = () => {
-    console.log(number)
-    console.log(subtitles[number])
-  }
  
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Player
-        controls={true}
-        component={MyComposition}
-        durationInFrames={2450}
-        compositionWidth={1080}
-        compositionHeight={1920}
-        fps={30}
-        className='border border-2 border-white/50 rounded-xl'
-      />
-      <div className='flex flex-col items-center justify-center gap-4 mt-4'>
-        <input className='border border-2 border-white/50 p-2' type='number' onChange={(e) => { setNumber(e.target.value) }} />
-        <button className='border border-2 border-white/50 p-2' onClick={handleCheck}>check</button>
-      </div>
+      
+      <h1>Render Eternal Impact</h1>
 
     </div>
   );
